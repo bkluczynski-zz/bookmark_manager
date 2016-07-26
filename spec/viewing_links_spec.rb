5499ff4db8d8b3ee4701e30cdb8ecbc79d8c2d9a
Bart <kluczynskib@gmail.com>
2016-07-26 14:46:09 +0200

    Challenge number 8. The test fails due to uninitilized constant Link

---
+++ spec/viewing_links_spec.rb
@@ -0,0 +1,13 @@
+feature 'viewing links' do
+  scenario 'so I can see existing links on the lings page' do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+
+    visit '/links'
+
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
+  end
+end
