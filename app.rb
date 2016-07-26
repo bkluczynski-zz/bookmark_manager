d2d91a5774890c747925113e7cb00cbc8c6ed9b3
Bart <kluczynskib@gmail.com>
2016-07-26 18:30:49 +0200

    Challenge 11
    As a time-pressed user
    So that I can save a website
    I would like to add the site's address and title to my bookmark manager

--- app.rb
+++ app.rb
@@ -3,9 +3,22 @@ require_relative 'models/link'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    'Hello Bookmark_manager!'
+  end
+
   get '/links' do
     @links = Link.all
     erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    Link.create(title: params[:title], url: params[:url])
+  redirect '/links'
   end
 
   # start the server if ruby file executed directly
---
+++ spec/creating_links_spec.rb
@@ -0,0 +1,14 @@
+feature 'creating links' do
+  scenario "so I can save the link" do
+    visit '/links/new'
+    fill_in 'title', with: 'BBC'
+    fill_in 'url', with: 'http://www.bbc.com'
+    click_button 'Submit'
+
+      expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content('BBC')
+    end
+  end
+end
